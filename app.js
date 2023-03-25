3d12c728cb7628f495256648af1aa5d135c6cac6
Tirthangkar Das <tirthangkardas@gmail.com>
2023-03-25 14:45:10 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const path = require('path')
 const ejs=require('ejs')
+const engine = require('ejs-mate')
 const app=express();
+const sass = require('node-sass-middleware');
+
+
+app.use(
+    sass({
+        src: __dirname + '/styles',
+        dest: __dirname + '/public', 
+        debug: true                
+    })
+);
 
 const home_route=require('./routes/homeRoutes')
 
 app.set('view engine', 'ejs')
+app.engine('ejs', engine)
 app.set('views', path.join(__dirname, 'views/pages')) 
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '/public')))
 app.use(express.urlencoded({extended:true}))
 app.use(home_route)
 
